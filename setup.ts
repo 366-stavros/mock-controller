84d3f965ffa50993307cdd4b0d0777fe4342ea8f
366-konstantinossymeou <konstantinos.sy@gtgroup.io>
2021-09-03 12:48:38 +0300

    local-depositor template

--- setup.ts
+++ setup.ts
@@ -31,5 +31,90 @@ export const TemplateMap: Map<string, ApiObject> = new Map([
       })),
       'array3' : apiArray(100, apiArray('random', 'address.direction'))
     })
-  ]
+  ],
+[
+  'local-depositor', apiObject({
+    'id': 'datatype.number',
+    'client': apiObject({
+      'contact_id': 'datatype.number',
+      'client_id': 'datatype.number',
+      'country': apiObject({
+        'id': 'datatype.number',
+        'name': 'address.country',
+        'iso': 'address.countryCode',
+        'is_available': 'datatype.number',
+      }),
+      'name': 'name.firstName',
+      'email': 'internet.email',
+    }),
+    'affiliate': apiObject({
+      'contact_id': 'datatype.number',
+      'affiliate_id': 'datatype.number',
+      'country': apiObject({
+        'id': 'datatype.number',
+        'name': 'address.country',
+        'iso': 'address.countryCode',
+        'is_available': 'random.boolean',
+      }),
+      'name': 'name.firstName',
+      'email': 'internet.email',
+    }),
+    'nickname': apiArray('random', apiObject({
+        'id': 'datatype.number',
+        'local_depositor': 'datatype.number',
+        'nickname': 'name.firstName',
+        'enabled': 'random.boolean',
+      }
+      )),
+    'transaction_fees': 'datatype.number',
+    'display_currency_id': apiObject({
+      "id": "datatype.number",
+      "full_name": "finance.currencyName",
+      "name": "finance.currencyCode",
+      "mt5_name": "finance.currencyCode",
+      "logo": "system.fileName",
+      "number_of_digits": "datatype.number",
+      "is_available": 'random.boolean',
+      "affiliate_is_available": 'random.boolean',
+      "currency_type": "datatype.number",
+      "transaction_check_url": "internet.url",
+      "usdt_omni_transaction_check_url": null
+    }),
+    'supported_currency': apiArray('random', apiObject({
+      "id": "datatype.number",
+      "full_name": "finance.currencyName",
+      "name": "finance.currencyCode",
+      "mt5_name": "finance.currencyCode",
+      "logo": "system.fileName",
+      "number_of_digits": "datatype.number",
+      "is_available": 'random.boolean',
+      "affiliate_is_available": 'random.boolean',
+      "currency_type": "datatype.number",
+      "transaction_check_url": "internet.url",
+      "usdt_omni_transaction_check_url": null
+      })),
+      'minimum_deposit_usd': 'finance.amount',
+      'maximum_deposit_usd': 'finance.amount',
+      'credit_line_status': 'random.boolean',
+      'allow_credit_line_extention_request': 'random.boolean',
+      'local_depositor_wallet_id': 'datatype.number',
+      'credit_line_limit': 'finance.amount',
+      'credit_line_usage': 'random.boolean',
+      'credit_line_stages': 'random.boolean',
+      'terms_and_conditions': apiObject({
+        'terms_and_conditions_flag': 'random.boolean',
+        'terms_and_conditions_type': 'datatype.number',
+        'terms_and_conditions_content': 'lorem.paragraph',
+      }),
+      'credit_line_help': apiObject({
+        'credit_line_help_flag': 'random.boolean',
+        'credit_line_content': 'lorem.paragraph',
+      }),
+      'completed_transition_time': apiObject({
+        'completed_transition_time_flag': 'random.boolean',
+        'completed_transition_time': 'datatype.number',
+      }),
+      'status': 'datatype.number',
+  })
+]
 ])
